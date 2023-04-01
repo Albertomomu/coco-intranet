bccfb9ac45b53eddfb3df03e28b2129a44f6911a
Alberto Moreno <alberto.moreno2357@gmail.com>
2023-04-01 13:30:24 +0200

    feat: download documents

--- src/app/pages/documents/documents.page.ts
+++ src/app/pages/documents/documents.page.ts
@@ -3,8 +3,10 @@ import { getDownloadURL } from 'firebase/storage';
 import { DocumentsService } from 'src/app/core/services/documents.service';
 import { Browser } from '@capacitor/browser';
 import { Filesystem, Directory } from '@capacitor/filesystem';
+import {Http} from "@capacitor-community/http"
 import { DocumentViewer, DocumentViewerOptions } from '@awesome-cordova-plugins/document-viewer/ngx';
 import { Router } from '@angular/router';
+import { fileURLToPath } from 'url';
 
 
 @Component({
@@ -48,24 +50,51 @@ export class DocumentsPage implements OnInit {
     })
   }
 
-  async downloadDocument(documentUrl: string, documentName: string): Promise<void> {
-    const response = await fetch(documentUrl);
-    const blob = await response.blob();
-    const reader = new FileReader();
-    reader.readAsDataURL(blob);
-    reader.onloadend = () => {
-      const base64data = reader.result!.toString();
-      Filesystem.writeFile({
-        path: documentName,
-        data: base64data,
-        directory: Directory.Documents,
-      }).then(() => {
-        console.log(`El documento ${documentName} se ha descargado correctamente`);
-      }).catch((error: any) => {
-        console.error(`Error al descargar el documento: ${error}`);
+
+  downloadFile(documentUrl: string, documentName: string) {
+    
+    Http.downloadFile({
+      url: documentUrl,
+      filePath: `${Directory.Documents}/${documentName}`
+    }).then(async (entry) => {
+      console.log(`${Directory.Documents}/${documentName}`)
+      /*const alert = await this.alertCtrl.create({
+        header: 'Archivo descargado',
+        message: `El archivo ${entry.path} ha sido descargado en el directorio Descargas.`,
+        buttons: ['OK']
       });
-    };
+      await alert.present();*/
+    }).catch(async (error) => {
+      console.log(error)
+      /*const alert = await this.alertCtrl.create({
+        header: 'Error',
+        message: `Error al descargar el archivo: ${error.message}`,
+        buttons: ['OK']
+      });
+      await alert.present();*/
+    });
   }
 
+  async downloadFile1(documentUrl: string, documentName: string) {
+    try {
+      // Crear el directorio si no existe
+      const directoryPath = `${Directory.Documents}/CocoGlobalMedia`;
+      await Filesystem.mkdir({
+        path: directoryPath,
+        directory: Directory.Documents,
+        recursive: true // crea directorios anidados si no existen
+      });
 
+      // Descargar el archivo
+      const filePath = `${directoryPath}/${documentName}`;
+      await Http.downloadFile({
+        url: documentUrl,
+        filePath
+      });
+
+      console.log(`Archivo descargado en ${filePath}`);
+    } catch (error) {
+      console.log(`Error al descargar el archivo: ${error}`);
+    }
+  }
 }
