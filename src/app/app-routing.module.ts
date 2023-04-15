c287c4e2a70665561df3af900faaa227eaabb130
Alberto <alberto.moreno2357@gmail.com>
2023-04-15 13:10:32 +0200

    feat¨Auth Guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,68 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'inicio',
-    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+    loadChildren: () =>
+      import('./pages/inicio/inicio.module').then((m) => m.InicioPageModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'documents',
-    loadChildren: () => import('./pages/documents/documents.module').then( m => m.DocumentsPageModule)
+    loadChildren: () =>
+      import('./pages/documents/documents.module').then(
+        (m) => m.DocumentsPageModule
+      ),
+    canActivate: [AuthGuard],
   },
   {
     path: 'satisfaction-form',
-    loadChildren: () => import('./pages/satisfaction-form/satisfaction-form.module').then( m => m.SatisfactionFormPageModule)
+    loadChildren: () =>
+      import('./pages/satisfaction-form/satisfaction-form.module').then(
+        (m) => m.SatisfactionFormPageModule
+      ),
+    canActivate: [AuthGuard],
   },
   {
     path: 'document-viewer/:documentUrl',
-    loadChildren: () => import('./pages/document-viewer/document-viewer.module').then( m => m.DocumentViewerPageModule)
+    loadChildren: () =>
+      import('./pages/document-viewer/document-viewer.module').then(
+        (m) => m.DocumentViewerPageModule
+      ),
+    canActivate: [AuthGuard],
   },
   {
     path: 'satisfaction-form-success',
-    loadChildren: () => import('./pages/satisfaction-form-success/satisfaction-form-success.module').then( m => m.SatisfactionFormSuccessPageModule)
+    loadChildren: () =>
+      import(
+        './pages/satisfaction-form-success/satisfaction-form-success.module'
+      ).then((m) => m.SatisfactionFormSuccessPageModule),
+    canActivate: [AuthGuard],
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@angular/core';
-import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { from, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  isLoggedIn = false;
 
-  constructor() { }
+  constructor() {}
 
   async login(email: string, password: string): Promise<any> {
     const auth = getAuth();
-    //const userCredential = await signInWithEmailAndPassword(auth, email, password);
     return await signInWithEmailAndPassword(auth, email, password);
   }
 
-  async signOut(){
+  isAuthenticated(): boolean {
+    const auth = getAuth();
+    if (auth.currentUser != null) {
+      this.isLoggedIn = true;
+    }
+    return this.isLoggedIn;
+  }
+
+  async signOut() {
     const auth = getAuth();
     return await signOut(auth);
   }
