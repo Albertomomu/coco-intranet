1fd4a06bc24749807557b6041c2af662d4d96a0c
Alberto <alberto.moreno2357@gmail.com>
2023-04-17 18:18:18 +0200

    feat: making html & css create user form

--- src/app/pages/admin-dashboard/users/create-user/create-user.module.ts
+++ src/app/pages/admin-dashboard/users/create-user/create-user.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { CreateUserPage } from './create-user.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    CreateUserPageRoutingModule
+    ReactiveFormsModule,
+    CreateUserPageRoutingModule,
   ],
-  declarations: [CreateUserPage]
+  declarations: [CreateUserPage],
 })
 export class CreateUserPageModule {}
--- src/app/pages/admin-dashboard/users/create-user/create-user.page.ts
+++ src/app/pages/admin-dashboard/users/create-user/create-user.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsService } from 'src/app/core/services/forms.service';
 
 @Component({
   selector: 'app-create-user',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-user.page.scss'],
 })
 export class CreateUserPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  createUserForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
   }
 
+  ngOnInit() {}
+
+  buildForm() {
+    this.createUserForm = this.formBuilder.group({
+      serviceSatisfaction: [0, [Validators.required]],
+      expectatives: [
+        '',
+        [
+          Validators.minLength(10),
+          Validators.maxLength(20),
+          Validators.required,
+        ],
+      ],
+      clientService: [0, [Validators.required]],
+      recommendation: ['', [Validators.required]],
+      overallExperience: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(500),
+        ],
+      ],
+    });
+  }
+
+  submitSatisfaction() {
+    if (!this.createUserForm.valid) {
+      this.createUserForm.markAllAsTouched();
+      this.createUserForm.markAsDirty();
+      return;
+    }
+
+    console.warn(this.createUserForm.value);
+    //AQUI VA EL BACKEND
+    //this.formsService.submitForm(this.createUserForm.value);
+    //this.router.navigate(['/satisfaction-form-success']);
+  }
 }
