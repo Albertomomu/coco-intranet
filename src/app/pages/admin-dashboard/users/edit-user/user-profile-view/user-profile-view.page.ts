88acd9ebdf3111baa49f43f64bbacba7d4096160
Alberto <alberto.moreno2357@gmail.com>
2023-04-24 18:48:15 +0200

    fix: adding updateUserForm

--- src/app/pages/admin-dashboard/users/edit-user/user-profile-view/user-profile-view.page.ts
+++ src/app/pages/admin-dashboard/users/edit-user/user-profile-view/user-profile-view.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { DocumentsService } from 'src/app/core/services/documents.service';
 import { UsersService } from 'src/app/core/services/users.service';
 
 @Component({
@@ -8,23 +11,37 @@ import { UsersService } from 'src/app/core/services/users.service';
   styleUrls: ['./user-profile-view.page.scss'],
 })
 export class UserProfileViewPage implements OnInit {
+  updateUserForm: FormGroup;
   user: any = [];
 
   constructor(
     private usersService: UsersService,
-    private route: ActivatedRoute
-  ) {}
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private auth: AuthService,
+    private docService: DocumentsService
+  ) {
+    this.buildForm();
+  }
 
   ngOnInit() {
     this.getUser();
-    console.log(this.user);
+  }
+
+  buildForm() {
+    this.updateUserForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      logo: ['', [Validators.required]],
+    });
   }
 
   async getUser() {
     const uid = this.route.snapshot.paramMap.get('uid');
     const user = await this.usersService.getUser(uid);
     this.user = user;
-    this.getLogo();
+    await this.getLogo();
   }
 
   async getLogo() {
